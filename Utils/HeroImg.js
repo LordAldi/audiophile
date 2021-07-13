8edf97fd0817d56eb64e43e8d41cd72f38a02a80
LordAldi <aldianugra09@gmail.com>
2021-07-14 02:21:37 +0700

    add hero section tablet and desktop

---
+++ Utils/HeroImg.js
@@ -0,0 +1,4 @@
+import heroMobile from "../assets/home/mobile/image-header.jpg";
+import heroTablet from "../assets/home/tablet/image-header.jpg";
+import heroDesktop from "../assets/home/desktop/image-hero.jpg";
+export default { heroDesktop, heroMobile, heroTablet };
--- pages/index.js
+++ pages/index.js
@@ -2,30 +2,36 @@ import Head from "next/head";
 import Image from "next/image";
 import Button from "../components/Button/Button";
 import Typography from "../components/Typography/Typography";
-import headerMobile from "../assets/home/mobile/image-header.jpg";
+import Hero from "../Utils/HeroImg";
 export default function Home() {
   return (
     <main>
-      <div className="relative">
-        <div className="image-bg">
-          <Image src={headerMobile} layout="responsive" />
+      <section className="relative">
+        <div className="image-bg md:hidden">
+          <Image src={Hero.heroMobile} layout="responsive" />
         </div>
-        <div className="absolute top-0 bottom-0 text-center w-full  text-white px-6 z-50 flex items-center justify-center">
-          <div className="mt-20">
-            <Typography variant="overline" className="">
+        <div className="image-bg hidden md:block xl:hidden">
+          <Image src={Hero.heroTablet} layout="responsive" />
+        </div>
+        <div className="image-bg hidden md:hidden xl:block">
+          <Image src={Hero.heroDesktop} layout="responsive" />
+        </div>
+        <div className="absolute top-0 bottom-0 text-center xl:text-left w-full  text-white  z-50 flex items-center justify-center xl:justify-start contain">
+          <div className=" mt-20 md:max-w-sm ">
+            <Typography variant="overline" className="opacity-50">
               New Product
             </Typography>
             <Typography variant="h1" className="mt-4 mb-6">
               XX99 Mark II HeadphoneS
             </Typography>
-            <Typography variant="body" className="text-gray mb-6">
+            <Typography variant="body" className=" mb-6 md:px-2 opacity-75">
               Experience natural, lifelike audio and exceptional build quality
               made for the passionate music enthusiast.
             </Typography>
             <Button classname="w-auto">SEE PRODUCT</Button>
           </div>
         </div>
-      </div>
+      </section>
       <div>neuafuf</div>
     </main>
   );
